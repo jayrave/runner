1e940a2f102272046a9c3e691fe44f8d34938dac
Jayanthan Raveendiran <jayanthan.raveendiran@gmail.com>
2020-04-21 22:32:23 -0700

    add textures

--- src/graphics/textures.rs
+++ src/graphics/textures.rs
@@ -8,22 +8,26 @@ pub struct Textures<'a> {
     environment_texture: Texture<'a>,
 }
 
-impl Textures {
-    pub fn load_from_files(canvas: &WindowCanvas) -> Textures {
-        let texture_creator = canvas.texture_creator();
+impl<'a> Textures<'a> {
+    pub fn load_from_files(texture_creator: &'a TextureCreator<WindowContext>) -> Textures<'a> {
+        // TODO - Couldn't find a way to hold `creator` in the same struct too!
+        // Compiler kept complaining & no one my workarounds did anything :(
+        let character_texture =
+            Textures::load_from_file("character_tile_sheet.png", texture_creator);
+
+        let environment_texture =
+            Textures::load_from_file("environment_tile_sheet.png", texture_creator);
+
         Textures {
-            character_texture: Textures::load_from_file(
-                "character_tile_sheet.png",
-                &texture_creator,
-            ),
-            environment_texture: Textures::load_from_file(
-                "environment_tile_sheet.png",
-                &texture_creator,
-            ),
+            character_texture,
+            environment_texture,
         }
     }
 
-    fn load_from_file(filename: &str, texture_creator: &TextureCreator<WindowContext>) -> Texture {
+    fn load_from_file(
+        filename: &str,
+        texture_creator: &'a TextureCreator<WindowContext>,
+    ) -> Texture<'a> {
         texture_creator
             .load_texture(format!("assets/{}", filename))
             .expect(&format!("Couldn't load texture: {}", filename))
