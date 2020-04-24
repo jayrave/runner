d4d96ef982c5ae625af1f150bdb50e52e2cf837c
Jayanthan Raveendiran <jayanthan.raveendiran@gmail.com>
2020-04-24 00:26:59 -0700

    add `frame_count_animated` to FrameStepper

--- src/resources/frame_stepper.rs
+++ src/resources/frame_stepper.rs
@@ -5,19 +5,25 @@ use std::time::SystemTime;
 const MILLISECONDS_IN_A_FRAME: u8 = 16;
 
 pub struct FrameStepper {
-    last_frame_at: SystemTime,
-    frame_count_to_animate: u8,
+    last_update_at: SystemTime,
+    frame_count_animated: u64,
+    frame_count_to_animate: u64,
 }
 
 impl FrameStepper {
     pub fn new() -> FrameStepper {
         FrameStepper {
-            last_frame_at: SystemTime::now(),
+            last_update_at: SystemTime::now(),
+            frame_count_animated: 0,
             frame_count_to_animate: 0,
         }
     }
 
-    pub fn frame_count_to_animate(&self) -> u8 {
+    pub fn frame_count_animated(&self) -> u64 {
+        self.frame_count_animated
+    }
+
+    pub fn frame_count_to_animate(&self) -> u64 {
         self.frame_count_to_animate
     }
 
@@ -27,9 +33,13 @@ impl FrameStepper {
 
     // `frame_count_to_animate` will be reset for every call to `updated`
     pub fn update(&mut self) {
+        // Make sure to update `animated` count & reset `to_animate` count
+        self.frame_count_animated += self.frame_count_to_animate;
+        self.frame_count_to_animate = 0;
+
         // We want to make sure we advance our physics in deterministic steps
         // all the time to be hardware independent
-        let mut ms_elapsed = self.last_frame_at.elapsed().unwrap().as_millis();
+        let mut ms_elapsed = self.last_update_at.elapsed().unwrap().as_millis();
         let mut frame_count_to_animate = 0;
         while ms_elapsed > MILLISECONDS_IN_A_FRAME.into() {
             frame_count_to_animate += 1;
@@ -38,7 +48,7 @@ impl FrameStepper {
 
         if frame_count_to_animate > 0 {
             self.frame_count_to_animate = frame_count_to_animate;
-            self.last_frame_at += Duration::from_millis(
+            self.last_update_at += Duration::from_millis(
                 frame_count_to_animate as u64 * MILLISECONDS_IN_A_FRAME as u64,
             );
         }
