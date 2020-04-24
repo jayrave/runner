7143759638e0ff67c4b21c392e908f3a278d6541
Jayanthan Raveendiran <jayanthan.raveendiran@gmail.com>
2020-04-24 00:34:29 -0700

    animate ground based on how many frames have elapsed

--- src/systems/physics/ground_system.rs
+++ src/systems/physics/ground_system.rs
@@ -8,7 +8,29 @@ use specs::join::Join;
 use specs::{ReadExpect, ReadStorage, System, WriteStorage};
 use std::io::Write;
 
+const FRAMES_PER_ANIMATION: u8 = 2;
+
 pub struct GroundSystem;
+
+impl GroundSystem {
+    fn update(ground_storage: &ReadStorage<Ground>, drawable_storage: &mut WriteStorage<Drawable>) {
+        let mut x_offset: Option<i32> = None;
+        for (_, drawable) in (ground_storage, drawable_storage).join() {
+            if x_offset.is_none() {
+                x_offset = Some(match drawable.world_bounds.x() {
+                    // TODO - use window bounds for computing this
+                    -450 => 50,
+                    _ => -2,
+                });
+            }
+
+            if let Some(x_offset) = x_offset {
+                drawable.world_bounds.offset(x_offset, 0);
+            }
+        }
+    }
+}
+
 impl<'a> System<'a> for GroundSystem {
     type SystemData = (
         ReadExpect<'a, FrameStepper>,
@@ -17,19 +39,12 @@ impl<'a> System<'a> for GroundSystem {
     );
 
     fn run(&mut self, mut data: Self::SystemData) {
-        for _ in 1..=data.0.frame_count_to_animate() {
-            let mut x_offset: Option<i32> = None;
-            for (_, drawable) in (&data.1, &mut data.2).join() {
-                if x_offset.is_none() {
-                    x_offset = Some(match drawable.world_bounds.x() {
-                        // TODO - use window bounds for computing this
-                        -450 => 50,
-                        _ => -2,
-                    });
-                }
-
-                if let Some(x_offset) = x_offset {
-                    drawable.world_bounds.offset(x_offset, 0);
+        let start_frame_count = data.0.frame_count_animated();
+        let end_frame_count = start_frame_count + data.0.frame_count_to_animate();
+        if start_frame_count != end_frame_count {
+            for frame_count in start_frame_count..end_frame_count {
+                if frame_count % FRAMES_PER_ANIMATION as u64 == 0 {
+                    GroundSystem::update(&data.1, &mut data.2)
                 }
             }
         }
