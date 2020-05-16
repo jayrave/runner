1518785e0c19187a548c8e73efe035cb0b171e36
Jayanthan Raveendiran <jayanthan.raveendiran@gmail.com>
2020-05-16 15:19:11 -0700

    draw tiles in correct z-order

--- src/systems/rendering_system.rs
+++ src/systems/rendering_system.rs
@@ -12,6 +12,14 @@ use specs::World;
 use specs::{ReadExpect, ReadStorage, System};
 use std::convert::TryFrom;
 
+// Tiles should be drawn in a particular order to get the correct Z-index cheaply.
+// This should be changed if the tiles drawn are going to change
+const TILE_DRAW_ORDER: [TileSheet; 3] = [
+    TileSheet::Environment,
+    TileSheet::Enemy,
+    TileSheet::Character,
+];
+
 pub struct RenderingSystem<'a> {
     world_data: WorldData,
     canvas: WindowCanvas,
@@ -46,20 +54,27 @@ impl<'a> RenderingSystem<'a> {
         self.canvas.clear();
 
         let viewport = self.canvas.viewport();
-        for drawable in drawables_storage.join() {
-            let texture = match drawable.tile_data.tile_sheet {
-                TileSheet::Character => &self.textures.character_texture,
-                TileSheet::Enemy => &self.textures.enemy_texture,
-                TileSheet::Environment => &self.textures.environment_texture,
-            };
+        for tile_sheet in TILE_DRAW_ORDER.iter() {
+            for drawable in drawables_storage.join() {
+                if drawable.tile_data.tile_sheet == *tile_sheet {
+                    let texture = match drawable.tile_data.tile_sheet {
+                        TileSheet::Character => &self.textures.character_texture,
+                        TileSheet::Enemy => &self.textures.enemy_texture,
+                        TileSheet::Environment => &self.textures.environment_texture,
+                    };
 
-            self.canvas
-                .copy(
-                    texture,
-                    drawable.tile_data.bounds_in_tile_sheet,
-                    RenderingSystem::world_to_screen_coordinates(&drawable.world_bounds, &viewport),
-                )
-                .expect("Couldn't draw texture");
+                    self.canvas
+                        .copy(
+                            texture,
+                            drawable.tile_data.bounds_in_tile_sheet,
+                            RenderingSystem::world_to_screen_coordinates(
+                                &drawable.world_bounds,
+                                &viewport,
+                            ),
+                        )
+                        .expect("Couldn't draw texture");
+                }
+            }
         }
 
         self.canvas.present();
