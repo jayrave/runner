45c8d2c629d52d3fb23ebc3c9b44e64a4ff415e3
Jayanthan Raveendiran <jayanthan.raveendiran@gmail.com>
2020-05-25 18:28:54 -0700

    move components into their own files

--- src/components.rs
+++ src/components.rs
@@ -2,12 +2,12 @@ mod animatable;
 mod cloud;
 mod drawable;
 pub mod enemy;
+mod ground;
 pub mod input;
-mod marker_components;
 pub mod player;
 
 pub use animatable::Animatable;
 pub use cloud::Cloud;
 pub use drawable::Drawable;
 pub use enemy::Enemy;
-pub use marker_components::Ground;
+pub use ground::Ground;
