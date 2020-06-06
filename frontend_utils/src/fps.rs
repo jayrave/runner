1bd53371810c4eba58a8b6114bbfeb8c704d94a9
Jayanthan Raveendiran <jayanthan.raveendiran@gmail.com>
2020-06-06 13:11:14 -0700

    add frontend_utils module

---
+++ frontend_utils/src/fps.rs
@@ -0,0 +1,29 @@
+use instant::Instant;
+
+pub struct Fps {
+    frame_count_in_window: u32,
+    window_start: Instant,
+}
+
+impl Fps {
+    pub fn new() -> Self {
+        Fps {
+            frame_count_in_window: 0,
+            window_start: Instant::now(),
+        }
+    }
+
+    pub fn track(&mut self) {
+        self.frame_count_in_window += 1;
+        let now = Instant::now();
+        let window_elapsed_in_seconds = (now - self.window_start).as_secs_f32();
+
+        if window_elapsed_in_seconds > 1.0 {
+            let fps_in_window = self.frame_count_in_window as f32 / window_elapsed_in_seconds;
+            println!("FPS: {}", fps_in_window);
+
+            self.frame_count_in_window = 0;
+            self.window_start = now;
+        }
+    }
+}
