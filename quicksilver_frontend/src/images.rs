c960fc01a0813232167eb9608ce752dabaf9cfa1
Jayanthan Raveendiran <jayanthan.raveendiran@gmail.com>
2020-06-08 02:08:07 -0700

    quicksilver: join on futures instead of going one-by-one

--- quicksilver_frontend/src/images.rs
+++ quicksilver_frontend/src/images.rs
@@ -13,14 +13,24 @@ pub struct Images {
 
 impl Images {
     pub async fn load_from_files(graphics: &Graphics) -> Images {
+        let mut images = futures::future::join_all(vec![
+            Images::load_from_file("cloud_tile_sheet.png", graphics),
+            Images::load_from_file("character_tile_sheet.png", graphics),
+            Images::load_from_file("enemy_tile_sheet.png", graphics),
+            Images::load_from_file("icon_tile_sheet.png", graphics),
+            Images::load_from_file("letter_tile_sheet.png", graphics),
+            Images::load_from_file("platform_tile_sheet.png", graphics),
+            Images::load_from_file("number_tile_sheet.png", graphics),
+        ]).await.into_iter();
+
         Images {
-            cloud_texture: Images::load_from_file("cloud_tile_sheet.png", graphics).await,
-            character_texture: Images::load_from_file("character_tile_sheet.png", graphics).await,
-            enemy_texture: Images::load_from_file("enemy_tile_sheet.png", graphics).await,
-            icon_texture: Images::load_from_file("icon_tile_sheet.png", graphics).await,
-            letter_texture: Images::load_from_file("letter_tile_sheet.png", graphics).await,
-            platform_texture: Images::load_from_file("platform_tile_sheet.png", graphics).await,
-            number_texture: Images::load_from_file("number_tile_sheet.png", graphics).await,
+            cloud_texture: images.next().unwrap(),
+            character_texture: images.next().unwrap(),
+            enemy_texture: images.next().unwrap(),
+            icon_texture: images.next().unwrap(),
+            letter_texture: images.next().unwrap(),
+            platform_texture: images.next().unwrap(),
+            number_texture: images.next().unwrap(),
         }
     }
 
